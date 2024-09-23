1936de29ce0f9bbc46e30821cfa500de9ca52b26
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-09-23 18:07:52 +0300

    Update main `urls.py`

--- group_1portal/group_1portal/urls.py
+++ group_1portal/group_1portal/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portal.urls')),
     path('forum/', include('forum.urls')),
+    path('diary/', include("diary.urls")),
+    path('events/', include("events.urls")),
+    path('surveys/', include("surveys.urls")),
+    path('votes/', include("votes.urls")),
+    path('announcements/', include("announcements.urls")),
+    path('materials/', include("materials.urls")),
+    path('protfolio/', include("portfolio.urls")),
+    path('gallery/', include("gallery.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
